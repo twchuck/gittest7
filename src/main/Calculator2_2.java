7934692645b97cc2c2566587c842cbfbed8d8332
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-27 17:33:22 +0800

    add 2_2

---
+++ src/main/Calculator2_2.java
@@ -0,0 +1,17 @@
+package main;
+
+public class Calculator2_2 {
+
+	public static int add(int a, int b) {
+		return a + b + b + b;
+	}
+
+
+	public static int multiply(int a, int b) {
+		return a * b * b;
+	}
+
+	public static int div(int a, int b) {
+		return a / b / b;
+	}
+}
