ec11974a2ba7f98f103ceab8feeda97c81143262
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-27 17:34:57 +0800

    remove sub

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -6,10 +6,6 @@ public class Calculator {
 		return a + b + b + b;
 	}
 
-	public static int sub(int a, int b) {
-		return a - b - b - b - b;
-	}
-
 	public static int multiply(int a, int b) {
 		return a * b * b;
 	}
