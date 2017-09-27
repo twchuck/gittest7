c5e5b95464920b1ec79b8eb58d192186f116620b
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-27 17:34:26 +0800

    remove add

--- src/main/Calculator2.java
+++ src/main/Calculator2.java
@@ -2,10 +2,6 @@ package main;
 
 public class Calculator2 {
 
-	public static int add(int a, int b) {
-		return a + b + b + b;
-	}
-
 
 	public static int multiply(int a, int b) {
 		return a * b * b;
