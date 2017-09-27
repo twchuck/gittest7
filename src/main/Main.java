d555446abf9535a41330f80019fad3ffec91290f
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-27 17:36:32 +0800

    remove

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println(Calculator.add(1, 2));
-		System.out.println(Calculator.sub(1, 2));
 		System.out.println(Calculator.multiply(1, 2));
 	}
 
