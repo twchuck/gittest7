e35d002298e640fa0646a940ca6d6d370bfc1c48
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-28 10:26:12 +0800

    add add2

--- src/main/Calculator2_2.java
+++ src/main/Calculator2_2.java
@@ -5,6 +5,9 @@ public class Calculator2_2 {
 	public static int add(int a, int b) {
 		return a + b + b + b;
 	}
+	public static int add2(int a, int b) {
+		return a + b + b + b;
+	}
 
 
 	public static int div(int a, int b) {
